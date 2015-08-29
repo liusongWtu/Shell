250bdedabe256d90ce25f06689651f26332760ed
liusongWtu <997896026@qq.com>
2015-08-29 14:20:43 +0800

    1

---
+++ update-git-repos.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+REPO_DIR=~/Tools/Shell
+REPOS=(mono PlaygroundOSS) # ここに定期更新したいリポジトリの一覧を書く
+GIT_OPTS="fetch -a --prune origin"
+RETRY_INT=60
+LOG_FILE=~/Documents/MyLog/.update_git.log
+
+function fetch_git_reops {
+    for r in "${REPOS[@]}"
+    do
+        echo "fetching... ${r}"
+        cd $REPO_DIR/$r
+        git $GIT_OPTS
+    done
+}
+
+if [ ! -t 1 ]; then
+    exec 1>> $LOG_FILE 2>&1
+fi
+echo "Starting git repos update at `date`"
+# check network connectivity
+i=0
+while :
+do
+    i=$((i + 1))
+    QRES=`dig +time=5 +tries=1 +short github.com` ; QRET=$?
+    if [ $QRET -eq 0 -a -n "$QRES" ]; then
+        fetch_git_reops
+        exit 0
+    fi
+    if [ $i -eq 5 ]; then
+        echo "No network access. Giving up."
+        exit 9
+    fi
+    echo "No network access. Retrying in $RETRY_INT seconds."
+    sleep $RETRY_INT
+done
